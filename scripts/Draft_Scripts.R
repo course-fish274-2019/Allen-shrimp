40ef6fdd0b79991185680e99c620f39924fa63ea
allentpe <allentpe@uw.edu>
2019-11-15 16:27:22 -0800

    Drafting methods of data manipulation using the mutate function, most likely will only use data_1 for this week's project.

--- scripts/Draft_Scripts.R
+++ scripts/Draft_Scripts.R
@@ -4,3 +4,9 @@ library(ggplot2)
 
 data_1 <- read.csv("https://ndownloader.figshare.com/files/16851587")
 data_2 <- read.csv("https://ndownloader.figshare.com/files/16851584")
+
+data_1_Modified <- mutate(data_1, ratio = (cor.telson/cor.abdomen))
+
+#telson/abdomen 
+#lower ratio = telson and abdomen COR difference smaller
+#higher ratio = telson and abdomen COR differece bigger (telson COR bigger)
